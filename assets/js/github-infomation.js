bfa69bb379354ef98a9a3c914e2d6b2d373cac0b
OniAkuma96 <harrison.tudball@hotmail.com>
2021-03-30 18:55:06 +0000

    implement emailjs integration on contact page

--- assets/js/github-infomation.js
+++ assets/js/github-infomation.js
@@ -35,7 +35,7 @@ function repoInfomationHTML(repos) {
             <ul>
                 ${listItemsHTML.join("\n")}
             </ul>
-        </div>`
+        </div>`;
 }
 
 function fetchGitHubInfomation(event) {
@@ -66,6 +66,9 @@ function fetchGitHubInfomation(event) {
         }, function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`)
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
---
+++ assets/js/sendEmail.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+    emailjs.send("service_o822clx", "template_pwlrhks", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
+
+
